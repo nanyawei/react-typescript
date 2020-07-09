6fc48249d758ca0f372580360e6a431daa757072
wyn <weiyanan@quantibio.com>
2020-07-09 17:50:46 +0800

    refactor: 重构exlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,58 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'pligin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'reqct-app',
+    // 'airbnb'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint',
+    'react',
+  ],
   env: {
     browser: true,
+    commonjs: true,
     es2020: true,
+    jest: false,
     node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2018,
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   rules: {
-    'react/jsx-filename-extension': [
-      1,
-      {
-        'extensions': ['.ts', '.tsx']
-      }
-    ]
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error",
+    "react-hooks/exhaustive-deps": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "jsx-a11y/no-noninteractive-element-interactions": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'default-case': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   }
 };
