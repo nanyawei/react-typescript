669a34daef1ff7b125ef1463f33fe3e95494d5c5
wyn <weiyanan@quantibio.com>
2020-07-09 14:39:29 +0800

    refactor(页面): wIP(页面):

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { hot } from 'react-hot-loader/root';
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import Test from "./page/test";
 
 const App = React.memo(() => {
   console.log(process.env);
 
-return <div>
-    {process.env.NODE_ENV}
-    <br/>
-    {process.env.APP_BASE_URL}
-    <br/>
-    hello ts
-    <br/>
-    hello react
-  </div>
+  return (
+    <div>
+      {process.env.NODE_ENV}
+      <br />
+      {process.env.APP_BASE_URL}
+      <br />
+      hello ts
+      <br />
+      hello react
+      <Test />
+    </div>
+  );
 });
 
 export default hot(App);
