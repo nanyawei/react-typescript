c9b24e1f780fbb291b9acdefafd75f41ea3c7e78
wyn <weiyanan@quantibio.com>
2020-07-09 17:54:29 +0800

    fix: 修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'pligin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
     'prettier',
